39ad18565da64bef9e0de3c082092efd61ca9512
Sergey <maybah__94@mail.ru>
2024-08-23 20:06:23 +0600

    first commit

---
+++ EffectiveToDo/EffectiveToDo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EffectiveToDo
+//
+//  Created by Мария Изюменко on 23.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
